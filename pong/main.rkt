61901627d82f810e10e543980ec863d4a6279b56
ym555 <mohamed.yoyo100@gmail.com>
2019-01-17 18:58:09 +0200

    smth

--- pong/main.rkt
+++ pong/main.rkt
@@ -1,32 +1,46 @@
+#!/usr/bin/env racket
 #lang racket/gui
 
 (define frame (new frame% [label "Pong"]))
 
 (define pong%
   (class canvas%
-	#| apparently needs to be the first thing |#
+	;; apparently needs to be the first thing
 	(super-new)
 
 	(define dc (send this get-dc))
-	(define player-one 10)
-	(define player-two 10)
 
-	(define (move-player-one y) (set! player-one (+ player-one y)))
-	(define (move-player-two y) (set! player-two (+ player-two y)))
+	(send dc set-brush "white" 'solid)
+	(send dc set-pen (new pen% [style 'transparent]))
+	(send dc set-smoothing 'aligned)
+	(send this set-canvas-background (make-object color% 0 0 0))
+
+	(define player-one 0)
+	(define player-two 0)
+
+	;; Better way to do this lol? macro? Box? match?
+	(define (move-player-one y) (set! player-one (modulo (+ player-one y) (send this get-height))))
+	(define (move-player-two y) (set! player-two (modulo (+ player-two y) (send this get-height))))
+
+	(define/override (on-size width height)
+	  (set! player-one (modulo player-one height))
+	  (set! player-two (modulo player-two height))
+	  (send this refresh-now))
 
 	(define/override (on-char event)
 	  (match (send event get-key-code)
-		['up  (move-player-one -1)(send this refresh-now)]
-		['down  (move-player-one 1) (send this refresh-now)]
-		[#\h  (move-player-two 1) (send this refresh-now)]
-		[#\t  (move-player-two -1) (send this refresh-now)]
-		[else '()]))
+		['up  (move-player-one -5)(send this refresh-now)]
+		['down  (move-player-one 5) (send this refresh-now)]
+		[#\h  (move-player-two 5) (send this refresh-now)]
+		[#\t  (move-player-two -5) (send this refresh-now)]
+		['escape (exit)]
+		[else void]))
 
 	(define/override (on-paint)
 	  (println 'repaint)
-	  (send dc set-smoothing 'aligned)
-	  (send dc draw-rectangle 1 player-one 25 50)
-	  (send dc draw-rectangle (( λ () (print (send this get-client-size)) 10)) player-two 25 50 ))))
+	  (send dc draw-ellipse 50 50 100 100)
+	  (send dc draw-rectangle 0 player-one 10 50)
+	  (send dc draw-rectangle (- (send this get-width) 10)  player-two 10 50))))
 
 (new pong% [parent frame])
 (send frame show #t)
